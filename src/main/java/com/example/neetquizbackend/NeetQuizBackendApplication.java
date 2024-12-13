aa386dacdb4eacebd03cabf393462e0961f33deb
codebyoshama <162036964+codebyoshama@users.noreply.github.com>
2024-12-13 02:04:17 -0600

    first commit

---
+++ src/main/java/com/example/neetquizbackend/NeetQuizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.neetquizbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeetQuizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeetQuizBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/neetquizbackend/NeetQuizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neetquizbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeetQuizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
